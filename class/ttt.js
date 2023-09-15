124d4c5f6c9abf0ff49ccdee299e43b6d1e1957c
Thi Phuc Thinh Huynh <ph.thinh905@gmail.com>
2023-09-14 21:43:53 -0700

    A small part of Phase 2

---
+++ class/ttt.js
@@ -0,0 +1,19 @@
+const Cursor = require("./cursor.js");
+const Screen = require("./screen.js");
+
+class TTT { // TTT is responsible for game play
+    constructor(playerTurn, grid, cursor) {
+        this.playerTurn = 0;
+        this.grid;
+        this.cursor = new Cursor;
+    }
+
+    // Screen.initialized();
+    // Screen.gridLines();
+    // Screen.message();
+    // Screen.backgroundColors();
+
+}
+
+
+module.exports = TTT;
